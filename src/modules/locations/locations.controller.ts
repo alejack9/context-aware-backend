06399c35d5d2bc25b439233e31dc22101aafcbc6
Alessandro Giacche <alejack9@users.noreply.github.com>
2021-08-10 21:06:32 +0200

    accept multiple values in getAverageNoises

--- src/modules/locations/locations.controller.ts
+++ src/modules/locations/locations.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Get,
@@ -17,12 +18,29 @@ export class LocationsController {
   constructor(private readonly locationsService: LocationsService) {}
 
   @Get()
-  async getAverageNoise(
-    @Query('lat') lat: number,
-    @Query('long') long: number,
+  async getAverageNoises(
+    @Query('lats') latsString: string,
+    @Query('longs') longsString: string,
   ) {
-    this.logger.log(`New average noise request from: ${lat},${long}.`);
-    return await this.locationsService.getAverageNoise(lat, long);
+    let lats: number[], longs: number[];
+    try {
+      lats = latsString.split(',').map((i) => +i);
+      longs = longsString.split(',').map((i) => +i);
+    } catch {
+      throw new BadRequestException('Not all passed values are numbers.');
+    }
+    if (lats.length !== longs.length)
+      throw new BadRequestException(
+        'latitudes and logitudes must be the same quantity',
+      );
+    this.logger.log(`New multiple average noise request.`);
+    const toRet = await Promise.all(
+      lats.map(
+        async (lat, i) =>
+          await this.locationsService.getAverageNoise(lat, longs[i]),
+      ),
+    );
+    return toRet;
   }
 
   @Post()
