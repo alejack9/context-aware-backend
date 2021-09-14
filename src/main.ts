be713d4a899b16dc0594b7a78447934d0a6077d8
Michele Celozzi <michele.celozzi97@gmail.com>
2021-09-14 13:21:09 +0200

    Env variables in windows

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config({path: __dirname+"/./../alwaysdata.env"})
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
