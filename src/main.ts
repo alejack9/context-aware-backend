7040c6d392df71e1e04e4951e6e9d66ebafe1e49
Alessandro Giacche <alejack9@users.noreply.github.com>
2021-09-15 12:39:55 +0200

    clean code and minor changes

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
-dotenv.config({path: __dirname+"/./../alwaysdata.env"})
+dotenv.config({ path: __dirname + '/./../alwaysdata.env' });
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger:
+      process.env.NODE_ENV === 'development'
+        ? ['log', 'debug', 'error', 'verbose', 'warn']
+        : ['error', 'warn'],
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
