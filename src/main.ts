72fe0bf5d67b21351b7d89165aa005530f5ab302
Alessandro Giacche <alejack9@users.noreply.github.com>
2021-09-16 02:10:02 +0200

    minor fix

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
 dotenv.config({ path: __dirname + '/./../alwaysdata.env' });
+const environment = process.env.NODE_ENV || 'development';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger:
-      process.env.NODE_ENV === 'development'
+      environment === 'development'
         ? ['log', 'debug', 'error', 'verbose', 'warn']
         : ['error', 'warn'],
   });
