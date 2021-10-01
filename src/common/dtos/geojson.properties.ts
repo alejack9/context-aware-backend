69cdc109c6ae9afee0781bb5da83d4f1f6041ae5
Michele Celozzi <michele.celozzi97@gmail.com>
2021-10-01 17:32:09 +0200

    Added default privacy parameters

--- src/common/dtos/geojson.properties.ts
+++ src/common/dtos/geojson.properties.ts
@@ -1,13 +1,16 @@
 import { GeoJsonProperties } from 'geojson';
+import * as defaultPrivacyParameters from './../../common/default-privacy-parameters';
 
-export interface DomainGeoJsonProperties extends GeoJsonProperties {
+export class DomainGeoJsonProperties implements GeoJsonProperties {
   timeStamp: number;
   noiseLevel: number;
 
-  dummyLocation: boolean;
-  gpsPerturbated: boolean;
-  perturbatorDecimals: number;
-  dummyUpdatesCount: number;
-  dummyUpdatesRadiusMin: number;
-  dummyUpdatesRadiusMax: number;
+  dummyLocation: boolean = defaultPrivacyParameters.dummyUpdatesEnabled;
+  gpsPerturbated: boolean = defaultPrivacyParameters.perturbatorEnabled;
+  perturbatorDecimals: number = defaultPrivacyParameters.perturbatorDecimals;
+  dummyUpdatesCount: number = defaultPrivacyParameters.dummyUpdatesCount;
+  dummyUpdatesRadiusMin: number =
+    defaultPrivacyParameters.dummyUpdatesRadiusMin;
+  dummyUpdatesRadiusMax: number =
+    defaultPrivacyParameters.dummyUpdatesRadiusMax;
 }
