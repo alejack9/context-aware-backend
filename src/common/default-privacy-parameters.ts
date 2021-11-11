de10fc4b41c6332b21bee42679ffdca22eded30d
Michele Celozzi <michele.celozzi97@gmail.com>
2021-11-11 22:49:17 +0100

    added more default parameters

--- src/common/default-privacy-parameters.ts
+++ src/common/default-privacy-parameters.ts
@@ -1,6 +1,6 @@
 export const perturbatorEnabled = false;
-export const perturbatorDecimals = 3;
+export const perturbatorDecimals = [1, 2, 3, 4, 5];
 export const dummyUpdatesEnabled = false;
 export const dummyUpdatesCount = 10;
-export const dummyUpdatesRadiusMin = 0.0005;
-export const dummyUpdatesRadiusMax = 0.004;
+export const dummyUpdatesRadiusMin = [0.0007, 0.0006, 0.0005, 0.0004, 0.0003];
+export const dummyUpdatesRadiusMax = [0.002, 0.003, 0.004, 0.005, 0.006];
