546abbb031cd10cc31652f885dcf4bd2a547c548
Alessandro Giacche <alejack9@users.noreply.github.com>
2021-12-07 19:16:07 +0100

    moved ormconfig to config folder

---
+++ src/config/orm.config.interface.ts
@@ -0,0 +1,10 @@
+export interface DatabaseConfig {
+  type: string;
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  database: string;
+  schema: string;
+  logging: boolean;
+}
--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -1,14 +1,12 @@
-module.exports = {
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => ({
   type: 'postgres',
   host: process.env.TYPEORM_HOST || 'localhost',
-  port: process.env.TYPEORM_PORT || 5432,
+  port: Number(process.env.TYPEORM_PORT) || 5432,
   username: process.env.TYPEORM_USERNAME || 'postgres',
   password: process.env.TYPEORM_PASSWORD || 'pass',
   database: process.env.TYPEORM_DATABASE || 'postgres',
   schema: 'project',
-  synchronize: true,
-  entities: ['dist/**/*.entity{.ts,.js}'],
   logging: false,
-  migrations: ['src/migration/**/*{.ts, .js}'],
-  subscribers: ['src/subscriber/**/*{.ts, .js}'],
-};
+}));
